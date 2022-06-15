49f499bca441638fc1ac83d11aa9bdd56dde158b
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-06-15 21:28:43 +0100

    Buffer additions and tweaks
    - Converted Experience namedtuple into dataclass
    - Created BufferParameters class, specific for Prioritized Experience Replay Buffers
    - Divided DQN parameters into separate dataclasses
    - Removed redundant code (EnvDetails into ReplayBuffer)
    - Added SumTree - binary tree data structure - as storage method for PrioritizedReplayBuffer
    - Added initial functionality for PrioritizedReplayBuffer

--- core/buffer.py
+++ core/buffer.py
@@ -1,12 +1,11 @@
 import random
+from typing import Union
 from collections import deque, namedtuple
+
+from core.parameters import BufferParameters, Experience
 
 import numpy as np
 import torch
-
-from core.env_details import EnvDetails
-
-Experience = namedtuple("Experience", field_names=['state', 'action', 'reward', 'next_state', 'done'])
 
 
 class ReplayBuffer:
@@ -14,15 +13,12 @@ class ReplayBuffer:
     A basic representation of an experience replay buffer.
 
     Parameters:
-        env_details (EnvDetails) - a class containing parameters for the environment
         buffer_size (int) - size of the memory buffer
         batch_size (int) - size of each training batch
         device (str) - device name for data calculations (CUDA GPU or CPU)
         seed (int) - a random number for recreating results
     """
-    def __init__(self, env_details: EnvDetails, buffer_size: int, batch_size: int,
-                 device: str, seed: int) -> None:
-        self.action_size = env_details.n_actions
+    def __init__(self, buffer_size: int, batch_size: int, device: str, seed: int) -> None:
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.seed = seed
@@ -65,6 +61,176 @@ class ReplayBuffer:
     def __get_experience(item: str, experiences: list) -> list:
         """Gets a single component of samples from a sample of experiences."""
         return [getattr(exp, item) for exp in experiences if exp is not None]
+
+    def __len__(self) -> int:
+        """Returns the current size of the buffer."""
+        return len(self.memory)
+
+
+class SumTree:
+    """A binary tree data structure for storing replay buffer transitions. Each leaf contains the
+    priority score for a single experience.
+
+    Parameters:
+        capacity (int) - size of tree memory (number of experiences to store)
+        device (str) - CUDA device to store the data on (CPU or GPU)
+    """
+    def __init__(self, capacity: int, device: str) -> None:
+        self.position = 0  # Pointer
+        self.capacity = capacity
+
+        self.num_nodes = 2 * capacity - 1  # Internal nodes
+        self.priorities = torch.zeros(self.num_nodes).to(device)  # All tree nodes
+        self.data = np.empty((capacity,), dtype=object)  # Data storage (leaf nodes)
+        self.max_priority = 1  # initial
+
+    def add(self, priority: int, experience: Experience) -> None:
+        """Add an experience to the tree with a given priority."""
+        # Add the experience to storage
+        self.data[self.position] = experience
+
+        # Update the tree
+        tree_idx = self.position + self.capacity - 1
+        self.__update_node(tree_idx, priority)
+
+        # Update pointer
+        self.position = (self.position + 1) % self.capacity
+
+        # Reset index if max capacity (overwrite existing)
+        if self.position >= self.capacity:
+            self.position = 0
+
+        # Update maximum priority
+        self.max_priority = max(priority, self.max_priority)
+
+    def __update_node(self, idx: int, priority: int) -> None:
+        """Adds priority value to respective node in the tree."""
+        self.priorities[idx] = priority
+        self.__update_tree(idx)
+        self.max_priority = max(priority, self.max_priority)  # Update max priority
+
+    def __update_tree(self, idx: Union[int, torch.Tensor]) -> None:
+        """Recursively propagates the new priority value(s) up the tree."""
+        # Handle single
+        if isinstance(idx, int):
+            self.__propagate_single(idx)
+        # Handle tensor
+        elif isinstance(idx, torch.Tensor):
+            self.__propagate_multi(idx)
+
+    def __propagate_single(self, idx: int) -> None:
+        """Propagates a single priority value up the tree."""
+        # Update parent node
+        parent_idx = (idx - 1) // 2
+        left_idx = 2 * parent_idx + 1
+        right_idx = left_idx + 1
+        self.priorities[parent_idx] = self.priorities[left_idx] + self.priorities[right_idx]
+
+        # Stop recursion when updated root
+        if parent_idx != 0:
+            self.__update_tree(parent_idx)
+
+    def __propagate_multi(self, indices: torch.Tensor) -> None:
+        """Propagates an array of priority values up the tree."""
+        # Get parent nodes
+        parent_indices = torch.div(indices - 1, 2, rounding_mode='floor')
+        unique_parents = torch.unique(parent_indices)  # indices
+
+        # Update parent priorities (sum of its child nodes)
+        child_indices = unique_parents * 2 + np.expand_dims([1, 2], axis=1)  # left and right
+        self.priorities[unique_parents] = torch.sum(self.priorities[child_indices], dim=0)
+
+        # Repeat until root has been updated
+        if parent_indices[0] != 0:
+            self.__update_tree(parent_indices)
+
+    def update(self, indices: torch.Tensor, priorities: torch.Tensor) -> None:
+        """Update one or more indices with new priorities."""
+        # Set new values and update tree
+        self.priorities[indices] = priorities
+        self.__update_tree(indices)
+
+        # Update maximum priority
+        current_max = torch.max(priorities).item()
+        self.max_priority = max(current_max, self.max_priority)
+
+    def __tree_search(self, indices: torch.Tensor, priorities: torch.Tensor) -> torch.Tensor:
+        """Obtains the indices of the given priorities by searching the tree."""
+        # Get left and right node indices - row[0] = left, row[1] = right
+        child_indices = (indices * 2 + np.expand_dims([1, 2], axis=1))  # shape -> [2, batch_size]
+
+        # If indices are leaf nodes, return them
+        if child_indices[0, 0] >= self.priorities.shape[0]:
+            return indices  # shape -> [batch_size,]
+
+        # Compare priorities to left node
+        left_child_values = self.priorities[child_indices[0]]
+        comparison_bools = torch.gt(priorities, left_child_values).to(torch.long)  # priority > left_child = 1, else 0
+
+        # Move down tree based on comparison -> 0 = left node, 1 = right node
+        next_indices = child_indices[comparison_bools, torch.arange(indices.size()[0])]
+
+        # Reduce priorities to move down tree
+        next_priorities = priorities - (comparison_bools * left_child_values)
+
+        # Recursively run until get leaf indices
+        return self.__tree_search(next_indices, next_priorities)
+
+    def sample(self, priorities: torch.Tensor) -> dict:
+        """Samples an array of values from the tree.
+        Returns the priority values, data index, and tree indices."""
+        init_indices = torch.zeros(priorities.shape, dtype=torch.long)  # Start for recursive tree search
+        indices = self.__tree_search(init_indices, priorities)
+        data_idx = indices - self.num_nodes
+        return {
+            'priorities': self.priorities[indices],
+            'transitions': self.data[data_idx],
+            'transition_indices': data_idx,
+            'priority_indices': indices
+        }
+
+    def total_priority(self) -> float:
+        """Returns the total priority (root node value)."""
+        return self.priorities[0].cpu().item()
+
+    def __len__(self) -> int:
+        return np.count_nonzero(self.data)
+
+
+class PrioritizedReplayBuffer:
+    """
+    A prioritized experience replay buffer. As illustrated in the Prioritized Experience Replay paper:
+    https://arxiv.org/pdf/1511.05952.pdf.
+
+    Parameters:
+        params (BufferParameters) - a class containing parameters for the buffer
+        device (str) - CUDA device name for data calculations (GPU or CPU)
+    """
+    def __init__(self, params: BufferParameters, device: str) -> None:
+        self.params = params
+        self.capacity = params.buffer_size
+        self.batch_size = params.batch_size
+        self.device = device
+
+        self.max_priority = 1
+        self.priority_weight = params.priority_weight
+        self.priority_weight_increase: float = 0.
+
+        self.memory = SumTree(self.capacity, device)
+
+    def add(self, priority: int, experience: Experience) -> None:
+        """Add a single experience to the buffer memory."""
+        self.memory.add(priority, experience)
+
+    def init_priority_weight_increase(self, num_episodes: int) -> None:
+        """Initializes the priority weight increase."""
+        self.priority_weight_increase = (1 - self.priority_weight) / (num_episodes - self.params.replay_period)
+
+    def sample(self) -> tuple[torch.Tensor, ...]:
+        """Randomly sample a batch of experiences from memory."""
+        # Anneal importance sampling weight (β to 1)
+        self.priority_weight = min(self.priority_weight + self.priority_weight_increase, 1)
+        pass
 
     def __len__(self) -> int:
         """Returns the current size of the buffer."""
--- core/parameters.py
+++ core/parameters.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from models._base import BaseModel
 
+import torch
 import torch.optim as optim
 import torch.nn.modules as modules
 
@@ -31,17 +32,31 @@ class AgentParameters:
 
 
 @dataclass
-class DQNParameters(AgentParameters):
-    """A data class for DQN parameters."""
+class CoreDQNParameters(AgentParameters):
+    """A data class containing the core DQN parameters."""
     gamma: float  # Discount factor
     tau: float  # Soft updater for target network
-    buffer_size: int  # Replay buffer size
-    batch_size: int  # Buffer training batch size
+    buffer_size: int  # Size of memory buffer
+    batch_size: int  # Buffer mini-batch size
     update_steps: int  # How often to update the network
+    max_timesteps: int = 1000  # Max before episode end
+
+
+@dataclass
+class DQNParameters(CoreDQNParameters):
+    """A data class for DQN parameters."""
     eps_start: float = 1.0  # Initial epsilon
     eps_end: float = 0.01  # Greedy epsilon threshold
     eps_decay: float = 0.995  # Epsilon decay rate
-    max_timesteps: int = 1000  # Max before episode end
+
+
+@dataclass
+class RainbowDQNParameters(CoreDQNParameters):
+    """A data class for Rainbow DQN parameters."""
+    # Categorical DQN
+    n_atoms: int = 51  # Number of atoms
+    v_min: int = -10  # Minimum size of the atoms
+    v_max: int = 10  # Maximum size of the atoms
 
 
 @dataclass
@@ -56,3 +71,24 @@ class PPOParameters(AgentParameters):
     entropy_coef: float = 0.01  # Coefficient for regularisation
     value_loss_coef: float = 0.5  # Coefficient for decreasing value loss
     clip_grad: float = 0.5  # Maximum value for gradient clipping
+
+
+@dataclass
+class BufferParameters:
+    """A data class containing the parameters for Prioritized Experience Replay Buffers."""
+    buffer_size: int  # Size of memory buffer
+    batch_size: int  # Buffer mini-batch size
+    priority_exponent: float  # Prioritized buffer exponent (alpha)
+    priority_weight: float  # Initial prioritized buffer importance sampling weight (beta)
+    replay_period: int  # Number of transitions before learning begins
+    n_steps: int  # Number of steps for multi-step learning
+
+
+@dataclass
+class Experience:
+    """A data class to store transitions (experiences)."""
+    state: torch.Tensor
+    action: int
+    reward: float
+    next_state: torch.Tensor
+    done: bool
